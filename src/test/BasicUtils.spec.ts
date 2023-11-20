28956a986a6b7687d68d5d3035f73a4e6e9787d9
Gilles Bruno <gilles_bruno@gmx.fr>
2023-11-20 12:29:06 +0100

    More better AAA phases

--- src/test/BasicUtils.spec.ts
+++ src/test/BasicUtils.spec.ts
@@ -2,8 +2,17 @@ import { product } from "../app/BasicUtils"
 
 describe('BasicUtils Test Suite', () => {
     it('Should return the product of 3 and 2', () => {
-        const actual = product(3, 2)
+        //Arrange
+        const input1 = 3
+        const input2 = 2
         const expected = 6
+        const testProduct = product
+
+        //Act
+        const actual = testProduct(input1, input2)
+
+        //Assert
         expect(actual).toBe(expected)
+        
     })
 })
