5434c928fa95a2ae13ff9c0ef16c9bff5d138d67
Gilles Bruno <gilles_bruno@gmx.fr>
2023-12-08 17:35:41 +0100

    remove useless semicolon

--- src/app/users/UserManager.ts
+++ src/app/users/UserManager.ts
@@ -1,43 +1,43 @@
 type User = {
-    id: number;
-    name: string;
-    email: string;
-  };
+    id: number
+    name: string
+    email: string
+  }
   
 export class UserManager {
-private users: User[] = [];
+private users: User[] = []
 
 addUser(user: User): void {
     if (this.users.some((u) => u.id === user.id)) {
-    throw new Error("User ID already exists");
+    throw new Error("User ID already exists")
     }
     if (this.users.some((u) => u.email === user.email)) {
-    throw new Error("User email already exists");
+    throw new Error("User email already exists")
     }
-    this.users.push(user);
+    this.users.push(user)
 }
 
 removeUser(id: number): void {
-    this.users = this.users.filter((user) => user.id !== id);
+    this.users = this.users.filter((user) => user.id !== id)
 }
 
 findUserById(id: number): User | undefined {
-    return this.users.find((user) => user.id === id);
+    return this.users.find((user) => user.id === id)
 }
 
 updateUser(id: number, updatedData: Partial<User>): void {
-    const userIndex = this.users.findIndex((user) => user.id === id);
+    const userIndex = this.users.findIndex((user) => user.id === id)
     if (userIndex === -1) {
-    throw new Error("User not found");
+    throw new Error("User not found")
     }
-    this.users[userIndex] = { ...this.users[userIndex], ...updatedData };
+    this.users[userIndex] = { ...this.users[userIndex], ...updatedData }
 }
 
 searchUsersByName(name: string): User[] {
-    return this.users.filter((user) => user.name.includes(name));
+    return this.users.filter((user) => user.name.includes(name))
 }
 
 getAllUsers(): User[] {
-    return this.users;
+    return this.users
 }
 }
