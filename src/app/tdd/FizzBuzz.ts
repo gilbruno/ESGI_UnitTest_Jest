6c073e7ba954f08663d4d0806010032e4a61f887
Gilles Bruno <gilles_bruno@gmx.fr>
2023-12-13 10:49:50 +0100

    Add test

--- src/app/tdd/FizzBuzz.ts
+++ src/app/tdd/FizzBuzz.ts
@@ -2,12 +2,12 @@
 export class FizzBuzz {
 
     generate(number: number) {
-        if (number % 15 === 0) 
-            return 'FizzBuzz'
         if (number % 3 === 0) 
             return 'Fizz'
         if (number % 5 === 0) 
             return 'Buzz'
+        if (number % 15 === 0) 
+            return 'FizzBuzz'
         return number.toString()
     }
 }
