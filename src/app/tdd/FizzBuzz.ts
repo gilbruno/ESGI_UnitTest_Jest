752daed240f9b2e7e6334a80e4bc52f7960fa022
Gilles Bruno <gilles_bruno@gmx.fr>
2023-12-13 17:45:25 +0100

    refacto fizzbuzz class

--- src/app/tdd/FizzBuzz.ts
+++ src/app/tdd/FizzBuzz.ts
@@ -4,24 +4,23 @@ export class FizzBuzz {
     generate(minNumber: number, maxNumber: number) {
         let result = ''
 
-        if (minNumber % 15 === 0) 
-            result += 'FizzBuzz'
-        else if (minNumber % 3 === 0) 
-            result += 'Fizz'
-        else if (minNumber % 5 === 0) 
-            result += 'Buzz'
-        else 
-            result += minNumber
-
+        result += this.evaluateNumber(minNumber)
         if (minNumber < maxNumber) {
-            if (maxNumber % 15 === 0) 
-            result += 'FizzBuzz'
-        else if (maxNumber % 3 === 0) 
-            result += 'Fizz'
-        else if (maxNumber % 5 === 0) 
-            result += 'Buzz'
-        else 
-            result += maxNumber
+            result += this.evaluateNumber(maxNumber)
+        }
+        return result
+    }
+
+    evaluateNumber(num: number) {
+        let result: string
+        if (num % 15 === 0) 
+            result = 'FizzBuzz'
+        else if (num % 3 === 0) 
+            result = 'Fizz'
+        else if (num % 5 === 0) 
+            result = 'Buzz'
+        else {
+            result = num.toString()
         }
         return result
     }
